625afae5599f3a9cf6da867569a79fc45738cec5
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-22 11:56:15 +0200

    add new function to class item to write new records to the csv files

--- app/items.ts
+++ app/items.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import csvParser from "csv-parser";
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 import { GeneralType } from "./types";
+import path from "path";
 
 class Items {
   items: Array<GeneralType>;
@@ -18,8 +20,8 @@ class Items {
     const pipedFileContnet = fileContent.pipe(csvParser({ separator: ";" }));
 
     const end = new Promise((resolve) => {
-      pipedFileContnet.on("Items", (Items: GeneralType) =>
-        resolve(this.items.push(Items))
+      pipedFileContnet.on("data", (items: GeneralType) =>
+        resolve(this.items.push(items))
       );
     });
 
@@ -34,6 +36,22 @@ class Items {
     return this.items.filter((item: GeneralType) =>
       item.authors.split(",").includes(author as string)
     );
+  }
+
+  async addNewItem(item: Array<GeneralType>, fileName: string) {
+    const csvWriter = createCsvWriter({
+      path: path.join(__dirname, "..", "files", `${fileName}.csv`),
+      header: [
+        { id: "title", title: "title" },
+        { id: "isbn", title: "isbn" },
+        { id: "authors", title: "authors" },
+        { id: "description", title: "description" },
+      ],
+      fieldDelimiter: ";",
+      append: true,
+    });
+
+    await csvWriter.writeRecords([item]);
   }
 }
 
