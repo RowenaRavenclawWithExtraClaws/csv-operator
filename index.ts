1951afa622baa46e094a56d57a8e3604fcea5ac3
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-22 11:54:39 +0200

    add json parser middleware

--- index.ts
+++ index.ts
@@ -3,14 +3,20 @@ import cors from "cors";
 import { PORTNUMBER } from "./app/helpers";
 import { getEndpoints } from "./app/endpoints";
 import { postEndpoints } from "./app/endpoints";
+import { Endpoint } from "./app/types";
 
 const app = express();
 
 // middlewares
 app.use(cors());
+app.use(express.json());
 
 // endpoints
-getEndpoints.forEach((endpoint) => app.get(endpoint.url, endpoint.handler));
-postEndpoints.forEach((endpoint) => app.post(endpoint.url, endpoint.handler));
+getEndpoints.forEach((endpoint: Endpoint) =>
+  app.get(endpoint.url, endpoint.handler)
+);
+postEndpoints.forEach((endpoint: Endpoint) =>
+  app.post(endpoint.url, endpoint.handler)
+);
 
 app.listen(PORTNUMBER, () => console.log(`listening on port ${PORTNUMBER}`));
