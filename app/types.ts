f57154086645e3ebaa2be80b52f50c4c632d639e
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-09-22 10:31:57 +0200

    tweak GenralType definition

--- app/types.ts
+++ app/types.ts
@@ -1,24 +1,24 @@
-export interface GeneralTypes {}
-
-export interface Author extends GeneralTypes {
+export interface Author {
   email: string;
   firstname: string;
   lastname: string;
 }
 
-export interface Book extends GeneralTypes {
+export interface Book {
   title: string;
   isbn: string;
   authors: string;
   description: string;
 }
 
-export interface Magazine extends GeneralTypes {
+export interface Magazine {
   title: string;
   isbn: string;
   authors: string;
   publishedAt: string;
 }
+
+export interface GeneralType extends Author, Book, Magazine {}
 
 type Handler = (req: Express.Request, res: Express.Response) => void;
 
